0d1b6db26dd4c858ccd2c5a2007e44ff04ee5707
nikhilmaske-2001 <59444243+nikhilmaske-2001@users.noreply.github.com>
2022-12-24 20:10:44 +0530

    COnnected to mongoDB.

--- api/index.js
+++ api/index.js
@@ -1,5 +1,17 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+    .then(console.log("Connected to MongoDB"))
+    .catch((error) => console.log(error));
+
 
 app.listen('5000', () => {
     console.log("Backend is running");
