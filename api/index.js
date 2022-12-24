9e83a8700a35d0cddb555a018474f5852c919e60
nikhilmaske-2001 <59444243+nikhilmaske-2001@users.noreply.github.com>
2022-12-24 20:31:17 +0530

    Api for register new user.

--- api/index.js
+++ api/index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const authRoute = require("./routes/auth")
 
 dotenv.config();
+app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
@@ -12,6 +14,7 @@ mongoose.connect(process.env.MONGO_URL, {
     .then(console.log("Connected to MongoDB"))
     .catch((error) => console.log(error));
 
+app.use("/api/auth", authRoute);
 
 app.listen('5000', () => {
     console.log("Backend is running");
