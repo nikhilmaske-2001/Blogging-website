669f164a5d8185ab7c1293fe197ae8a8ada8c907
nikhilmaske-2001 <59444243+nikhilmaske-2001@users.noreply.github.com>
2022-12-25 08:26:20 +0530

    Delete a post.

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -34,6 +34,24 @@ router.put("/:id", async (req, res) => {
     }
 });
 
+// delete my post
+router.delete("/:id", async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if (post.username === req.body.username) {
+            try {
+                await post.delete()
+                res.status(200).json("Post has been deleted");
+            } catch (error) {
+                res.status(500).json(error);
+            }
+        } else {
+            res.status(401).json("You can delete only your post");
+        }
+    } catch (error) {
+    }
+});
+
 
 
 
