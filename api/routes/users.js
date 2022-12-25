b106b18232f951211ddd747a1fc182d2c0a9535b
nikhilmaske-2001 <59444243+nikhilmaske-2001@users.noreply.github.com>
2022-12-25 07:59:02 +0530

    Get a user api.

--- api/routes/users.js
+++ api/routes/users.js
@@ -47,4 +47,15 @@ router.delete("/:id", async (req, res) => {
     }
 });
 
+// Get a user
+router.get("/:id", async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        const { password, ...others } = user._doc;
+        res.status(200).json(others);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+})
+
 module.exports = router;
