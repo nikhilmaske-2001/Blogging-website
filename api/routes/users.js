0e19a89566a5f3edb8a261bfcc983ede25b656f2
nikhilmaske-2001 <59444243+nikhilmaske-2001@users.noreply.github.com>
2022-12-25 07:53:18 +0530

    Delete user api.

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
+const Post = require("../models/Post");
 
 // Update
 router.put("/:id", async (req, res) => {
@@ -22,6 +23,27 @@ router.put("/:id", async (req, res) => {
         }
     } else {
         res.status(401).json("You can update only your account")
+    }
+});
+
+
+// Delete User with their posts
+router.delete("/:id", async (req, res) => {
+    if (req.body.userId === req.params.id) {
+        try {
+            const user = await User.findById(req.params.id);
+            try {
+                await Post.deleteMany({ username: user.username });
+                await User.findByIdAndDelete(req.params.id);
+                res.status(200).json("User has been deleted");
+            } catch (error) {
+                res.status(500).json(error);
+            }
+        } catch (error) {
+            res.status(404).json("User not found!");
+        }
+    } else {
+        res.status(401).json("You can delete only your account")
     }
 });
 
