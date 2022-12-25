bc88e85e607ee0daa74ad363ba08dbb3b71c45d6
nikhilmaske-2001 <59444243+nikhilmaske-2001@users.noreply.github.com>
2022-12-25 17:51:11 +0530

    Register frontend backend connected.

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,4 +1,5 @@
 import "./login.css";
+import { Link } from "react-router-dom";
 
 export default function Login() {
   return (
@@ -6,12 +7,24 @@ export default function Login() {
       <span className="loginTitle">Login</span>
       <form className="loginForm">
         <label>Email</label>
-        <input className="loginInput" type="text" placeholder="Enter your email..." />
+        <input
+          className="loginInput"
+          type="text"
+          placeholder="Enter your email..."
+        />
         <label>Password</label>
-        <input className="loginInput" type="password" placeholder="Enter your password..." />
+        <input
+          className="loginInput"
+          type="password"
+          placeholder="Enter your password..."
+        />
         <button className="loginButton">Login</button>
       </form>
-        <button className="loginRegisterButton">Register</button>
+      <button className="loginRegisterButton">
+        <Link className="link" to="/register">
+          Register
+        </Link>
+      </button>
     </div>
   );
 }
