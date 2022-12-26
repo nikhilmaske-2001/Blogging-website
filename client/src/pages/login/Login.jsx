201bddafab57981696b2b9f5233ab6454ef713da
nikhilmaske-2001 <59444243+nikhilmaske-2001@users.noreply.github.com>
2022-12-26 20:37:58 +0530

    Context API>

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,26 +1,50 @@
 import "./login.css";
 import { Link } from "react-router-dom";
+import { useContext, useRef } from "react";
+import { Context } from "../../context/Context";
+import axios from "axios";
 
 export default function Login() {
+  const userRef = useRef();
+  const passwordRef = useRef();
+  const { user, dispatch, isFetching } = useContext(Context);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    dispatch({ type: "LOGIN_START" });
+    try {
+      const res = await axios.post("/auth/login", {
+        username: userRef.current.value,
+        password: passwordRef.current.value,
+      });
+      dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
+    } catch (error) {
+      dispatch({ type: "LOGIN_FAILURE" });
+    }
+  };
+  console.log(user);
+
   return (
     <div className="login">
       <span className="loginTitle">Login</span>
-      <form className="loginForm">
-        <label>Email</label>
+      <form className="loginForm" onSubmit={handleSubmit}>
+        <label>Username</label>
         <input
           className="loginInput"
           type="text"
-          placeholder="Enter your email..."
+          placeholder="Enter your Username..."
+          ref={userRef}
         />
         <label>Password</label>
         <input
           className="loginInput"
           type="password"
           placeholder="Enter your password..."
+          ref={passwordRef}
         />
         <button className="loginButton">Login</button>
       </form>
-      <button className="loginRegisterButton">
+      <button type="submit" className="loginRegisterButton">
         <Link className="link" to="/register">
           Register
         </Link>
